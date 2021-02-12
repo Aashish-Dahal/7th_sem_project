96da3ca782f64133561c989480dc6006069ae863
Aashish-Dahal <ashishdahal490@gmail.com>
2021-02-12 13:54:05 +0545

    Different MeatProductList Created

---
+++ masu_bazar flutter/lib/screens/widgets/mutton.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:masu_bazar/screens/widgets/chickenItemModel/meatItemModel.dart';
+import 'package:masu_bazar/screens/widgets/productGrid.dart';
+
+class MuttonList extends StatelessWidget {
+  final List<MeatItemModel> _muttonItem = MeatItemModel.getMuttonItems;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.orange,
+          elevation: 0.0,
+          title: Text(
+            "Mutton Items",
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CircleAvatar(
+                  radius: 20.0,
+                  backgroundColor: Colors.orange,
+                  child: Image(
+                    image: AssetImage('assets/images/logo1.png'),
+                  )),
+            ),
+          ],
+        ),
+        body: ProductGrid(
+          pList: _muttonItem,
+        ));
+  }
+}
---
+++ masu_bazar flutter/lib/screens/widgets/productGrid.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import 'chickenItemModel/meatItemModel.dart';
+import 'productDetail.dart';
+
+class ProductGrid extends StatelessWidget {
+  final List<MeatItemModel> pList;
+  const ProductGrid({Key key, this.pList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 2,
+      children: pList
+          .map((item) => InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (builder) => ProductDetails(
+                                image: item.image,
+                                productName: item.name,
+                              )));
+                },
+                child: Container(
+                    decoration: BoxDecoration(boxShadow: [
+                      BoxShadow(
+                          color: Colors.white38, offset: Offset(0.0, 20.0))
+                    ]),
+                    child: Card(
+                      elevation: 10.0,
+                      child: Column(
+                        children: <Widget>[
+                          Image(
+                            image: AssetImage(item.image),
+                            height: 140,
+                            width: 200,
+                            fit: BoxFit.fill,
+                          ),
+                          SizedBox(
+                            height: 5.0,
+                          ),
+                          Text(
+                            item.name,
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    )),
+              ))
+          .toList(),
+    );
+  }
+}
