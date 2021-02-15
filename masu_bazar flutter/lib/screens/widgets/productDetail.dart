83cac70c5d9053e95248282ecd504aacb3d969da
Aashish-Dahal <ashishdahal490@gmail.com>
2021-02-15 12:52:31 +0545

    Increment and decrement

--- masu_bazar flutter/lib/screens/widgets/productDetail.dart
+++ masu_bazar flutter/lib/screens/widgets/productDetail.dart
@@ -12,6 +12,17 @@ class ProductDetails extends StatefulWidget {
 }
 
 class _PrductDetailsState extends State<ProductDetails> {
+  double _quantity = 0.0;
+  double _price = 300;
+  double delCharge = 10;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,6 +39,7 @@ class _PrductDetailsState extends State<ProductDetails> {
       body: ListView(
         children: [
           Container(
+            margin: EdgeInsets.only(left: 5, right: 5),
             decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(10.0),
@@ -37,10 +49,183 @@ class _PrductDetailsState extends State<ProductDetails> {
                   BoxShadow(color: Colors.black26, offset: Offset(0.0, 1.0)),
                 ]),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 _ImageDetailSection(image: widget.image),
-                _TextDetailSection(name: widget.productName),
+                // _TextDetailSection(
+                //   name: widget.productName,
+                // ),
+                Expanded(
+                  child: Container(
+                    color: Colors.black,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          child: Text(
+                            widget.productName,
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 5),
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black12),
+                              // color: Colors.purple,
+                              borderRadius: BorderRadius.circular(12.0)),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                'Quantity:',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                              Container(
+                                  height: 30,
+                                  width: 30,
+                                  child: IconButton(
+                                      icon: Icon(
+                                        Icons.remove,
+                                        size: 15,
+                                        color: Colors.white,
+                                      ),
+                                      onPressed: () {
+                                        setState(() {
+                                          if (_quantity > 1) {
+                                            _quantity -= 0.5;
+                                          } else {
+                                            //nothing
+                                          }
+                                        });
+                                      }),
+                                  decoration: BoxDecoration(
+                                    color: Colors.orange,
+                                    borderRadius: BorderRadius.circular(50),
+                                  )),
+                              Text('$_quantity kg'),
+                              Container(
+                                  height: 30,
+                                  width: 30,
+                                  child: IconButton(
+                                      icon: Icon(
+                                        Icons.add,
+                                        size: 15,
+                                        color: Colors.white,
+                                      ),
+                                      onPressed: () {
+                                        setState(() {
+                                          _quantity += 0.5;
+                                        });
+                                      }),
+                                  decoration: BoxDecoration(
+                                    color: Colors.orange,
+                                    borderRadius: BorderRadius.circular(50),
+                                  )),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 5),
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black12),
+                              // color: Colors.purple,
+                              borderRadius: BorderRadius.circular(12.0)),
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(left: 8.0),
+                                child: Text(
+                                  'Price:',
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 8.0),
+                                child: Text(
+                                    '${_priceIncrement(price: _price, quantity: _quantity)} '),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 5),
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black12),
+                              // color: Colors.purple,
+                              borderRadius: BorderRadius.circular(12.0)),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 8.0, left: 8.0),
+                                child: Text(
+                                  'Delivery Charge: $delCharge',
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 5),
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black12),
+                              // color: Colors.purple,
+                              borderRadius: BorderRadius.circular(12.0)),
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 8.0, left: 8.0),
+                                child: Text(
+                                  'Total Price:',
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 8.0, left: 8.0),
+                                child: Text(
+                                    '${_totalPriceIncrement(price: _priceIncrement(price: _price, quantity: _quantity), Tquantity: _quantity, delCharge: delCharge)}'),
+                              )
+                            ],
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            print('hellele');
+                          },
+                          child: Container(
+                            height: 30,
+                            width: 150,
+                            margin: EdgeInsets.only(top: 15),
+                            decoration: BoxDecoration(
+                                color: Colors.orange,
+                                borderRadius: BorderRadius.circular(10.0),
+                                border: Border.all(
+                                    color: Colors.black26,
+                                    style: BorderStyle.solid),
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Colors.black26,
+                                      offset: Offset(0.0, 1.0)),
+                                ]),
+                            child: Center(
+                                child: Text(
+                              'CONFIRM ORDER',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
           )
@@ -63,153 +248,13 @@ Widget _ImageDetailSection({String image}) {
   );
 }
 
-Widget _TextDetailSection({String name}) {
-  return Expanded(
-    child: Container(
-      height: 200,
-      child: Column(
-        //  crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-            child: Text(
-              name,
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 5),
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black12),
-                // color: Colors.purple,
-                borderRadius: BorderRadius.circular(12.0)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(
-                  'Quantity:',
-                  style: TextStyle(fontSize: 16),
-                ),
-                Container(
-                    height: 30,
-                    width: 30,
-                    child: IconButton(
-                        icon: Icon(
-                          Icons.remove,
-                          size: 15,
-                          color: Colors.white,
-                        ),
-                        onPressed: () {}),
-                    decoration: BoxDecoration(
-                      color: Colors.orange,
-                      borderRadius: BorderRadius.circular(50),
-                    )),
-                Text('0.0 kg'),
-                Container(
-                    height: 30,
-                    width: 30,
-                    child: IconButton(
-                        icon: Icon(
-                          Icons.add,
-                          size: 15,
-                          color: Colors.white,
-                        ),
-                        onPressed: () {}),
-                    decoration: BoxDecoration(
-                      color: Colors.orange,
-                      borderRadius: BorderRadius.circular(50),
-                    )),
-              ],
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 5),
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black12),
-                // color: Colors.purple,
-                borderRadius: BorderRadius.circular(12.0)),
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 8.0),
-                  child: Text(
-                    'Price:',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0, left: 8.0),
-                  child: Text("_______"),
-                )
-              ],
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 5),
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black12),
-                // color: Colors.purple,
-                borderRadius: BorderRadius.circular(12.0)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0, left: 8.0),
-                  child: Text(
-                    'Delivery Charge:1',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 5),
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black12),
-                // color: Colors.purple,
-                borderRadius: BorderRadius.circular(12.0)),
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0, left: 8.0),
-                  child: Text(
-                    'Total Price:',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0, left: 8.0),
-                  child: Text('______'),
-                )
-              ],
-            ),
-          ),
-          InkWell(
-            onTap: () {
-              print('hellele');
-            },
-            child: Container(
-              height: 30,
-              width: 150,
-              margin: EdgeInsets.only(top: 15),
-              decoration: BoxDecoration(
-                  color: Colors.orange,
-                  borderRadius: BorderRadius.circular(10.0),
-                  border: Border.all(
-                      color: Colors.black26, style: BorderStyle.solid),
-                  boxShadow: [
-                    BoxShadow(color: Colors.black26, offset: Offset(0.0, 1.0)),
-                  ]),
-              child: Center(
-                  child: Text(
-                'CONFIRM ORDER',
-                style: TextStyle(color: Colors.white),
-              )),
-            ),
-          )
-        ],
-      ),
-    ),
-  );
+double _priceIncrement({double price, quantity}) {
+  price = quantity * price;
+  return price;
+}
+
+double _totalPriceIncrement(
+    {double Tprice = 0.0, double price, double Tquantity, double delCharge}) {
+  Tprice = Tprice + price + delCharge;
+  return Tprice;
 }
